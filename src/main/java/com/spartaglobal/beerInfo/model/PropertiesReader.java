4313aa0df4844dda8d317da72c2cadc7ace1ca16
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-27 17:14:19 +0100

    changed names in properties file and created properties reader

---
+++ src/main/java/com/spartaglobal/beerInfo/model/PropertiesReader.java
@@ -0,0 +1,43 @@
+package com.spartaglobal.beerInfo.model;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesReader {
+    private static Properties properties;
+    private static  String baseURL;
+    private static String beersEndPoint;
+
+    private PropertiesReader() {
+
+    }
+
+
+    public static String getBaseURL() {
+        if(baseURL==null){
+            setUp();
+        }
+
+        return baseURL;
+    }
+
+    public static String getBeersEndPoint(){
+        if(beersEndPoint==null){
+            setUp();
+        }
+
+        return beersEndPoint;
+    }
+
+    private static void setUp(){
+        try {
+            properties = new Properties();
+            properties.load(new FileReader("resources/beer_info.properties"));
+            baseURL = properties.getProperty("base_URL");
+            beersEndPoint = properties.getProperty("beers_endpoint");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
