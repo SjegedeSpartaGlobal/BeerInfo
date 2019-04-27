8170f97dc8e36d9c55c014fce6c1e2b750fcbdc4
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-27 17:22:59 +0100

    created HTTP Manager to send request

---
+++ src/main/java/com/spartaglobal/beerInfo/model/HTTPManager.java
@@ -0,0 +1,43 @@
+package com.spartaglobal.beerInfo.model;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HTTPManager {
+
+    private CloseableHttpResponse fullResponse;
+
+    public HTTPManager() {
+        makeAllRatesCall();
+    }
+
+    //cornstructor methods
+    private void makeAllRatesCall() {
+        try {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet getLatestRates = new HttpGet(PropertiesReader.getBaseURL() + PropertiesReader.getBeersEndPoint());
+            System.out.println(PropertiesReader.getBaseURL() + PropertiesReader.getBeersEndPoint());
+            fullResponse = httpClient.execute(getLatestRates);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Accessor methods
+    public String getResponseBody() {
+        String bodyResult = null;
+
+        try {
+            bodyResult = EntityUtils.toString(fullResponse.getEntity());
+
+        } catch (IOException e) {
+
+        }
+        return bodyResult;
+    }
+}
