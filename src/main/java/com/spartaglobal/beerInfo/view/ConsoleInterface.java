24f500cab4dce94f19535f17ff2d196add8562ab
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-28 22:45:26 +0100

    completed console interface

--- src/main/java/com/spartaglobal/beerInfo/view/ConsoleInterface.java
+++ src/main/java/com/spartaglobal/beerInfo/view/ConsoleInterface.java
@@ -2,8 +2,7 @@ package com.spartaglobal.beerInfo.view;
 
 import com.spartaglobal.beerInfo.controller.BeerInfoServices;
 import com.spartaglobal.beerInfo.model.BeerInfoDTO.BeerInfoDTO;
-import com.spartaglobal.beerInfo.model.BeerInfoServices.BeerInfoService;
-import com.spartaglobal.beerInfo.model.BeerInfoServices.BeerSearcher;
+import com.spartaglobal.beerInfo.model.BeerInfoServices.*;
 import org.json.simple.JSONObject;
 
 import java.util.Scanner;
@@ -13,40 +12,26 @@ public class ConsoleInterface {
 
     private BeerInfoServices beerServices;
 
+    public void run(){
+        String input = getInput();
+        search(input);
+    }
+
     public void search(String searchTerm){
-
-        String input2 = "";
-        String input3 = "";
-
         if(searchTerm.equals("Search by Beer Name")){
-            System.out.println("Enter Beer Name: ");
-            input2 = getInput();
-            if(!isExit(input2)) {
-                BeerSearcher bs = beerServices.searchBeers(input2);
-                displayResults(bs.getBeerNames(), bs.getAmount(), input2);
-                System.out.println("Would you like to get the first beer or pick a random one? ");
-                input3 = getInput();
-                if(!isExit(input3)) {
-                    JSONObject jsonObject = getJSONObject(input3, bs);
-                    displayDTO(jsonObject);
-                } else{
-                    System.out.println("See you later!");
-                }
-            }else{
-                System.out.println("See you later!");
-            }
+            searchByName();
         }
         else if(searchTerm.equals("Search by Food")){
-
+            searchByFood();
         }
         else if(searchTerm.equals("Search by High Alchohol Content")){
-
+            searchByHighAlc();
         }else if(searchTerm.equals("Search by Low Alchohol Content")){
-
+            searchByLowAlc();
         }else if(searchTerm.equals("Search by High Bitterness Rating")){
-
+            searchByHighBitterness();
         }else if(searchTerm.equals("Search by Low Bitterness Rating")){
-
+            searchByLowBitterness();
         }
     }
     public String getInput(){
@@ -85,5 +70,93 @@ public class ConsoleInterface {
             return true;
         }
         return false;
+    }
+
+    public void searchHelper(BeerInfoService beerInfoService, String input){
+        displayResults(beerInfoService.getBeerNames(), beerInfoService.getAmount(), input);
+        System.out.println("Would you like to get the first beer or pick a random one? ");
+        input = getInput();
+        if(!isExit(input)) {
+            JSONObject jsonObject = getJSONObject(input, beerInfoService);
+            displayDTO(jsonObject);
+        } else{
+            System.out.println("See you later!");
+        }
+    }
+
+    public void searchByName(){
+        String input2 = "";
+        System.out.println("Enter Beer Name: ");
+        input2 = getInput();
+        if(!isExit(input2)) {
+            BeerSearcher bs = beerServices.searchBeers(input2);
+            searchHelper(bs, input2);
+        }else{
+            System.out.println("See you later!");
+        }
+    }
+
+    public void searchByFood(){
+        String input2 = "";
+        System.out.println("Enter Beer Name: ");
+        input2 = getInput();
+        if(!isExit(input2)) {
+            FoodToBeerFinder fb = beerServices.beersMatchingFood(input2);
+            searchHelper(fb, input2);
+        }else{
+            System.out.println("See you later!");
+        }
+    }
+
+    public void searchByHighAlc(){
+        String input2 = "";
+        System.out.println("Enter Beer Name: ");
+        input2 = getInput();
+        if(!isExit(input2)) {
+            int intInput = Integer.parseInt(input2);
+            GreaterAlchoholBeersRetreiver highAlc = beerServices.getHighAlcContent(intInput);
+            searchHelper(highAlc, input2);
+        }else{
+            System.out.println("See you later!");
+        }
+    }
+
+    public void searchByLowAlc(){
+        String input2 = "";
+        System.out.println("Enter Beer Name: ");
+        input2 = getInput();
+        if(!isExit(input2)) {
+            int intInput = Integer.parseInt(input2);
+            LowerAlchoholBeersRetriever lowAlc = beerServices.getLowAlcContent(intInput);
+            searchHelper(lowAlc, input2);
+        }else{
+            System.out.println("See you later!");
+        }
+    }
+
+    public void searchByLowBitterness(){
+        String input2 = "";
+        System.out.println("Enter Beer Name: ");
+        input2 = getInput();
+        if(!isExit(input2)) {
+            int intInput = Integer.parseInt(input2);
+            LowerBitternessBeersRetriever lowBitterness = beerServices.getLowBittenessBeers(intInput);
+            searchHelper(lowBitterness, input2);
+        }else{
+            System.out.println("See you later!");
+        }
+    }
+
+    public void searchByHighBitterness(){
+        String input2 = "";
+        System.out.println("Enter Beer Name: ");
+        input2 = getInput();
+        if(!isExit(input2)) {
+            int intInput = Integer.parseInt(input2);
+            GreaterBitternessBeersRetriever highBitterness = beerServices.getHighBittenessBeers(intInput);
+            searchHelper(highBitterness, input2);
+        }else{
+            System.out.println("See you later!");
+        }
     }
 }
